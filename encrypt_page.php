2c77d4283627914de5dd96321def7cd44a60f1bd
gregorymayo <gregoryjuevesii23@gmail.com>
2019-12-11 21:35:50 -0800

    add DES cipher

--- encrypt_page.php
+++ encrypt_page.php
@@ -18,6 +18,7 @@ _END;
     require_once 'regular_function.php';
     require_once 'simpleSub_key.php';
     require_once 'rc4_key.php';
+    require_once 'rc4_function.php';
     //Create A Connection With Database
 	global $conn;
 	$conn = new mysqli($hn, $un, $pw, $db);
@@ -160,6 +161,81 @@ _END;
     //encryption for Double Transposition
     function encryptionDES($input){
         
+        $pt= "123456ABCD132536"; 
+        $key= "AABB09182736CCDD"; 
+        //Hex to binary 
+        $key= hex2binn($key); 
+        //Parity bit drop table 
+        $keyp = array (  
+            57,49,41,33,25,17,9, 
+            1,58,50,42,34,26,18, 
+            10,2,59,51,43,35,27, 
+            19,11,3,60,52,44,36,           
+            63,55,47,39,31,23,15, 
+            7,62,54,46,38,30,22, 
+            14,6,61,53,45,37,29, 
+            21,13,5,28,20,12,4 
+        );
+        //getting 56 bit key from 64 bit using the parity bits 
+        $key= permute($key, $keyp, 56);
+        // echo strlen($key);
+        //echo $key;
+        
+        //Number of bit shifts  
+        $shift_table  = array(
+            1, 1, 2, 2, 
+            2, 2, 2, 2,  
+            1, 2, 2, 2,  
+            2, 2, 2, 1 
+        );  
+        //Key- Compression Table 
+        $key_comp  = array( 
+            14,17,11,24,1,5, 
+            3,28,15,6,21,10, 
+            23,19,12,4,26,8, 
+            16,7,27,20,13,2, 
+            41,52,31,37,47,55, 
+            30,40,51,45,33,48, 
+            44,49,39,56,34,53, 
+            46,42,50,36,29,32 
+        );
+        //Splitting 
+        $left = substr($key, 0, 28);
+        //echo "<br> $left";
+        $right = substr($key, 28, 28);
+        //echo "<br> $right";
+        $inputrkb = array();
+        $inputrk = array();
+        
+        for($i=0;$i<16;$i++){
+            //Shifting 
+            $left= shiftLeft($left, $shift_table[$i]); 
+            $right= shiftLeft($right, $shift_table[$i]); 
+            
+            //Combining 
+            $combine = "";
+            $combine .= $left;
+            $combine .= $right;
+            //echo "<br> $combine";
+            //Key Compression 
+            $RoundKey= permute($combine, $key_comp, 48); 
+            //echo "<br> $RoundKey";
+            // $inputrkb.push_back($RoundKey); 
+            // $inputrk.push_back(bin2hex($RoundKey));
+            array_push($inputrkb,$RoundKey); 
+            array_push($inputrk,bin2hexx($RoundKey));
+            //echo "<br> $inputrkb[$i] $i susu";
+            //echo "<br> $inputrk[$i] $i kuda";
+        }
+        //print_r($inputrkb);
+        //print_r($inputrk);
+        
+        $finalOut = encrypt($pt,$inputrkb,$inputrk);
+
+        echo "<br><br>$finalOut";
+        /*
+        return $finalOut;
+        */
     }
     
 ?>
